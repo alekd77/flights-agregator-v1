ea6ec78f1a2127ee3b0d8a8ff87d015feef627e2
adul <aleksander.dul.77@gmail.com>
2026-01-16 01:03:17 +0100

    Added JsonCreator annotation and constructors to RegularFlightDtoV1 and RegularFlightPriceDtoV1

--- flights-common/src/main/java/com/example/flights/common/dto/RegularFlightDtoV1.java
+++ flights-common/src/main/java/com/example/flights/common/dto/RegularFlightDtoV1.java
@@ -1,5 +1,6 @@
 package com.example.flights.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -11,12 +12,26 @@ import java.time.Duration;
 @Builder
 public class RegularFlightDtoV1 {
     private final String id;
-    @JsonProperty("departureCode")
     private final String deptCode;
-    @JsonProperty("destinationCode")
     private final String destCode;
-    @JsonProperty("dateTime")
     private final LocalDateTime deptDateTime;
     private final RegularFlightPriceDtoV1 price;
     private final Duration duration;
+
+    @JsonCreator
+    public RegularFlightDtoV1(
+            @JsonProperty("id") String id,
+            @JsonProperty("deptCode") String deptCode,
+            @JsonProperty("destCode") String destCode,
+            @JsonProperty("deptDateTime") LocalDateTime deptDateTime,
+            @JsonProperty("price") RegularFlightPriceDtoV1 price,
+            @JsonProperty("duration") Duration duration) {
+        this.id = id;
+        this.deptCode = deptCode;
+        this.destCode = destCode;
+        this.deptDateTime = deptDateTime;
+        this.price = price;
+        this.duration = duration;
+    }
+
 }
--- flights-common/src/main/java/com/example/flights/common/dto/RegularFlightPriceDtoV1.java
+++ flights-common/src/main/java/com/example/flights/common/dto/RegularFlightPriceDtoV1.java
@@ -1,5 +1,6 @@
 package com.example.flights.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Builder;
 import lombok.Data;
 import java.math.BigDecimal;
@@ -9,4 +10,12 @@ import java.math.BigDecimal;
 public class RegularFlightPriceDtoV1 {
     private final BigDecimal amount;
     private final String currency;
+
+    @JsonCreator
+    public RegularFlightPriceDtoV1(
+            BigDecimal amount,
+            String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 }
